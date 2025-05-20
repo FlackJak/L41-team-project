8ff59d59d18e06dd1761c1ca630a6d5ff79e5b84
Aryan Maheshwary <aryanmaheshwary0875@gmail.com>
2025-05-20 16:31:38 +0530

    Added Community,Header, 4 images, contact

---
+++ community.js
@@ -0,0 +1,10 @@
+// community.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const joinBtn = document.querySelector(".btn-primary");
+    joinBtn.addEventListener("click", () => {
+      alert("Redirecting to Bluestock Community Signup!");
+      // Example: window.location.href = "/signup";
+    });
+  });
+  
---
+++ contact.js
@@ -0,0 +1,19 @@
+function signIn() {
+    window.location.href = "index.html";
+  }
+  
+  function signUp() {
+    window.location.href = "sign.html";
+  }
+  
+  function openChat() {
+    alert("Launching chat support...");
+    // Simulate opening chat
+    window.location.href = "/support/chat";
+  }
+  
+  function joinCommunity() {
+    alert("Joining community...");
+    window.location.href = "/community";
+  }
+  
